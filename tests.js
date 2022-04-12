92556c6215684a0e52ce2f6bb7de8c5ad6b50ab6
“Cory <coryurton@gmail.com>
2022-04-12 09:34:52 -0500

    add the first test and solution for intro-to-testing

--- tests.js
+++ tests.js
@@ -13,3 +13,18 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+// Open up tests.js. Add a describe, an it, and an expect to assert that sayHello is a defined function.
+// Use your tests for helloWorld as a guide.
+
+describe('sayHello', function() {
+    it('should be a defined function', function () {
+        expect(typeof sayHello).toBe('function');
+    })
+    it("should never return 'undefined' when called", function () {
+        expect(sayHello()).not.toBe(undefined);
+    });
+});
+
+//Step 2: Run all the tests.
+// At this point, we're expecting and hoping for a single, red failing test that we just now authored.
